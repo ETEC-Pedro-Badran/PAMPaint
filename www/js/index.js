a8f4ec2c110435018c6ffdb2c66cbf82d35a6421
EVALDO FERNANDES RÉU JÚNIOR <EVALDO@DESKTOP-8UMJN9H>
2020-10-14 15:23:09 -0300

    Acionamento da câmera pelo menu lateral.

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,41 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-	
+    //console.log("Dispositivo pronto:");
+    //console.log(navigator.camera);
+
+    $("#mnFoto").click(function(){
+          const opcoes = {
+          };
+
+          var  sucesso = function(imageData){
+            let lista = $("#lista-fotos");
+
+            lista.append("<li><img src='data:image/jpeg;base64," + imageData+"'></li>");
+
+            
+            
+                
+
+
+          };
+          var erro = function(mensagem){
+              console.log(mensagem);
+          };
+
+
+          navigator.camera.getPicture(sucesso, erro, opcoes);
+
+    });
+
 }
+
+
+
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    //console.log("Interface Pronta:");
+    //console.log(navigator.camera);
+});
